8c3d66c7cf5e73f7e0a5bf08c64ce1d6d7aff3dc
Piotr Masek <piotr3km@gmail.com>
2016-06-05 21:25:15 +0200

    small HANDLE_ERROR change

--- kernel.cu
+++ kernel.cu
@@ -6,12 +6,12 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 
-static void HandleError(cudaError_t err,
-	const char *file,
-	int line) {
-	if (err != cudaSuccess) {
-		printf("%s in %s at line %d\n", cudaGetErrorString(err),
-			file, line);
+//error printing
+static void HandleError(cudaError_t err, const char *file, int line) 
+{
+	if (err != cudaSuccess) 
+	{
+		printf("%s in %s at line %d\n", cudaGetErrorString(err), file, line);
 		exit(EXIT_FAILURE);
 	}
 }
